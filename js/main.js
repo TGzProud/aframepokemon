335178a5b3b89ba6ee46d3c1c041e73014803b9a
Gijs Buis <gijsbuis@Gijss-MacBook-Pro.local>
2020-01-05 12:35:22 +0100

    added starter pokemon

--- js/main.js
+++ js/main.js
@@ -7,23 +7,33 @@ window.onload = () => {
   const pokemonTextTwo = document.getElementById('js--pokemon-text-two');
   const pokemonPictureThree = document.getElementById('js--pokemon-picture-three');
   const pokemonTextThree = document.getElementById('js--pokemon-text-three');
+  const pokemonPictureFour = document.getElementById('js--pokemon-picture-four');
+  const pokemonTextFour = document.getElementById('js--pokemon-text-four');
+
+  const bulbasaur = document.getElementById('js--bulbasaur');
+  const charmander = document.getElementById('js--charmander');
+  const squirtle = document.getElementById('js--squirtle');
 
   resetPokemon.onmouseenter = (event) => {
     setRandomPokemon();
   }
 
+  resetPokemon.onmouseleave = (event) => {
+    resetPokemon.setAttribute('color', 'blue');
+  }
+
+  // Get the random pokemon from the API
+
   function setRandomPokemon() {
     let randomPokemonNumber = Math.floor(Math.random() * 151 + 1);
     let randomPokemonNumberTwo = Math.floor(Math.random() * 151 + 1);
     let randomPokemonNumberThree = Math.floor(Math.random() * 151 + 1);
+    let randomPokemonNumberFour = Math.floor(Math.random() * 151 + 1);
     getPokemon(randomPokemonNumber);
     getPokemonTwo(randomPokemonNumberTwo);
     getPokemonThree(randomPokemonNumberThree);
+    getPokemonFour(randomPokemonNumberFour);
     resetPokemon.setAttribute('color', 'red');
-  }
-
-  resetPokemon.onmouseleave = (event) => {
-    resetPokemon.setAttribute('color', 'green');
   }
 
   const getPokemon = (numberOfPokemon) => {
@@ -60,6 +70,18 @@ window.onload = () => {
     .then( (response) => {
       pokemonPictureThree.setAttribute('src', response.sprites.front_default);
       pokemonTextThree.setAttribute('value', response.name);
+    });
+  }
+
+  const getPokemonFour = (numberOfPokemon) => {
+    const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+    fetch(BASE_URL + numberOfPokemon)
+    .then( (data) => {
+      return data.json();
+    })
+    .then( (response) => {
+      pokemonPictureFour.setAttribute('src', response.sprites.front_default);
+      pokemonTextFour.setAttribute('value', response.name);
     });
   }
 
